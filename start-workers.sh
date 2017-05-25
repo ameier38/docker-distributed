e1d4c1aa4231bcb87524f02f07e2f8381d08e849
Andrew Meier <ameier38@gmail.com>
2017-05-25 16:58:56 -0400

    simplify worker script

--- start-workers.sh
+++ start-workers.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-NCPUS=`python -c "import multiprocessing as mp; print(mp.cpu_count())"`
-START_PORT=50800
-echo "Detected $NCPUS cpus"
+WORKER_PORT=50800
+NANNY_PORT=50801
 source activate py3
-PORT=$START_PORT
-for (( i=0;i<$NCPUS;i++ ))
-do
-    worker_port=$PORT
-    ((PORT++))
-    nanny_port=$PORT
-    ((PORT++))
-    worker="worker_$i"
-    log_file=/$worker.log
-    echo "Starting $worker on port $worker_port"
-    (dask-worker scheduler:8786 --nthreads 1 --nprocs 1 --worker-port $worker_port --nanny-port $nanny_port) > $log_file 2>&1 &
-done
+dask-worker scheduler:8786 --worker-port $WORKER_PORT --nanny-port $NANNY_PORT
