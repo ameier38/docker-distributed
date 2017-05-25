f5fbdd9f066b8f24bbf1ada74012dfe643934f2b
Andrew Meier <ameier38@gmail.com>
2017-05-25 16:41:35 -0400

    update worker script

--- start-workers.sh
+++ start-workers.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 NCPUS=`python -c "import multiprocessing as mp; print(mp.cpu_count())"`
-START_PORT = 50800
+START_PORT=50800
 echo "Detected $NCPUS cpus"
 source activate py3
-for (( port=START_PORT;i<$NCPUS;port++ ))
+PORT=$START_PORT
+for (( i=0;i<$NCPUS;i++ ))
 do
-    dask-worker scheduler:8786 --nthreads 1 --nprocs 1 --worker-port $port
+    worker_port=$PORT
+    ((PORT++))
+    nanny_port=$PORT
+    ((PORT++))
+    worker="worker_$i"
+    log_file=/$worker.log
+    echo "Starting $worker on port $worker_port"
+    (dask-worker scheduler:8786 --nthreads 1 --nprocs 1 --worker-port $worker_port --nanny-port $nanny_port) > $log_file 2>&1 &
 done
